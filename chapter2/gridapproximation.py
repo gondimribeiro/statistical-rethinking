c86903fe0db35aec72d45c0397ae6541963e6f3d
gondimribeiro <gondimribeiro@gmail.com>
2017-03-23 13:03:24 -0300

    Updating

--- chapter2/gridapproximation.py
+++ chapter2/gridapproximation.py
@@ -2,10 +2,15 @@ import numpy as np
 import scipy.stats as ss
 import matplotlib.pyplot as plt
 
-n = 20
+n = 50
+W = 3
+T = 3
+
 grid = np.linspace(0, 1, n)
-prior = np.ones(n)
-likelihood = ss.binom(9, grid).pmf(6)
+
+prior = np.concatenate([np.zeros(int(n / 2)), 2*np.ones(int(n / 2))])
+
+likelihood = ss.binom(T, grid).pmf(W)
 unstd_posterior = likelihood * prior
 posterior = unstd_posterior / unstd_posterior.sum()
 
